d63dcc519c7a241bfb33fa6f2b9ad5a1906a49b5
iftekharweb <iftekharweb@gmail.com>
2023-07-10 19:41:07 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Head';
+import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <ToastContainer/>
+      <Container className='my-2'>
+        <Outlet/>
+      </Container>
+      
+    </>
+  )
+}
+
+export default App
